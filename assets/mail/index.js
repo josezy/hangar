befaa91f03ed91cc19181d3edc1a3357ce82a7a1
Jose B <jose.zdy@gmail.com>
2020-05-23 17:55:03 -0500

    contact.json file, add rate limiter

--- assets/mail/index.js
+++ assets/mail/index.js
@@ -2,17 +2,24 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const fs = require('fs')
+const rateLimit = require("express-rate-limit")
 
 const app = express()
 
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 1 // limit each IP to max requests per windowMs
+})
+
 app.use(cors())
+app.use(limiter)
 app.use(bodyParser.json())
 
 app.post('/contact', (req, res) => {
-    fs.readFile('contact.json', function (err, data) {
+    fs.readFile('contact.json', 'utf8', function (err, data) {
         let json = JSON.parse(data || '[]')
         json.push(req.body)
-        fs.writeFile("contact.json", JSON.stringify(json))
+        fs.writeFile("contact.json", JSON.stringify(json), err => {if(err) console.log(err)})
     })
     res.send({success: true})
 })
